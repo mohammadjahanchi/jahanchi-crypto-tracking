3d3c74a859bfff4da8ae84b25fa1622ab7b52f80
mohammad <mohammadjahanchi@gmail.com>
2023-09-07 17:04:05 +0330

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn1 React
         </a>
       </header>
     </div>
