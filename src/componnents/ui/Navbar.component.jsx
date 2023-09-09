d3532bd99fae1a77dfc9ea2cec5171195c6492a7
mohammad <mohammadjahanchi@gmail.com>
2023-09-09 11:40:51 +0330

    navbar 100% to complete

--- src/componnents/ui/Navbar.component.jsx
+++ src/componnents/ui/Navbar.component.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-
+import './navbar.css'
+import { Link } from "react-router-dom";
+import { FaCoins } from 'react-icons/fa'
 const NavbarComponent = () => {
     return (
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <div>
+      <FaCoins className="navbar-logo" />
+      <span className="navbar-logo-text">Crypto Tracking</span>
+      </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        <Link class="nav-link" to="faq">FAQ</Link>
+        <Link class="nav-link" to="about-us">about us</Link>
+        <Link class="nav-link" to="contact-us">contact us</Link>
+        
       </div>
     </div>
   </div>
