fad174f09987d322c958ee393aec65d08d5751f9
mohammad <mohammadjahanchi@gmail.com>
2023-09-09 22:32:58 +0430

    footer,navbar 95% to complate

--- src/componnents/ui/Navbar.component.jsx
+++ src/componnents/ui/Navbar.component.jsx
@@ -3,7 +3,10 @@ import './navbar.css'
 import { Link } from "react-router-dom";
 import { FaCoins } from 'react-icons/fa'
 const NavbarComponent = () => {
-    
+    const btnToggleRef=useRef();
+    const toggleMenue=()=>{
+      btnToggleRef.current.click();
+    }
     
     return (
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -12,15 +15,15 @@ const NavbarComponent = () => {
       <FaCoins className="navbar-logo" />
       <span className="navbar-logo-text">Crypto Tracking</span>
       </div>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" ref={btnToggleRef}>
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <Link class="nav-link active" aria-current="page" to="/"   >Home</Link>
-        <Link class="nav-link" to="faq" >FAQ</Link>
-        <Link class="nav-link" to="about-us"  >about us</Link>
-        <Link class="nav-link" to="contact-us"  >contact us</Link>
+        <Link class="nav-link active" aria-current="page" to="/" onClick={toggleMenue}  >Home</Link>
+        <Link class="nav-link" to="faq" onClick={toggleMenue} >FAQ</Link>
+        <Link class="nav-link" to="about-us" onClick={toggleMenue} >about us</Link>
+        <Link class="nav-link" to="contact-us" onClick={toggleMenue}  >contact us</Link>
         
       </div>
     </div>
