01a93bfa2579f697c436318174e87e805728c85a
mohammad <mohammadjahanchi@gmail.com>
2023-09-10 20:08:00 +0330

    about us 50% to compalet

--- src/pages/AboutUs.page.jsx
+++ src/pages/AboutUs.page.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 
-const AboutUs=()=>{
-    return(
-        <div>
-            <h5>Aboute Us</h5>
-            <h5>Aboute Us</h5>
-            <h5>Aboute Us</h5>
-            
-           
+import aboutImg from '../assets/images/about.png'
+const AboutUs = () => {
+    return (
+        <div className="container">
+            <div className="columns">
+                <div className="col-12 bg-info p-3 rounded ">
+                    <h3>About us</h3>
+                </div>
+            </div>
+            <div className="row mt-4">
+                <div className="col-md-6 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam pariatur voluptatum sint tenetur, quia dolore eius, ratione placeat corporis facilis accusamus quo dolores sed officia iure totam modi nemo error.</div>
+
+                <div className="col-md-6 bg-success ">
+                    <img src={aboutImg} alt="about"></img>
+                </div>
+
+            </div>
+
+
+
         </div>
+
+
     )
 };
 export default AboutUs;
