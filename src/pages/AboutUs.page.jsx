dd35592e716488ca5a0ab4d2b9c612ad4b5a28af
mohammad <mohammadjahanchi@gmail.com>
2023-09-11 16:09:02 +0330

    aboute us 100%

--- src/pages/AboutUs.page.jsx
+++ src/pages/AboutUs.page.jsx
@@ -9,11 +9,11 @@ const AboutUs = () => {
                     <h3>About us</h3>
                 </div>
             </div>
-            <div className="row mt-4">
-                <div className="col-md-6 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam pariatur voluptatum sint tenetur, quia dolore eius, ratione placeat corporis facilis accusamus quo dolores sed officia iure totam modi nemo error.</div>
+            <div className="row align-items-center ">
+                <div className="col-md-7 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam pariatur voluptatum sint tenetur, quia dolore eius, ratione placeat corporis facilis accusamus quo dolores sed officia iure totam modi nemo error.</div>
 
-                <div className="col-md-6 bg-success ">
-                    <img src={aboutImg} alt="about"></img>
+                <div className="col-md-5 text-center ">
+                    <img src={aboutImg} alt="about" className="img-fluid about-img"></img>
                 </div>
 
             </div>
