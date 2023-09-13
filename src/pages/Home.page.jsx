4913c63c733cfad97df6155aef833e7771832431
mohammad <mohammadjahanchi@gmail.com>
2023-09-13 19:41:36 +0330

    use state 30%

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import SPinner from "../componnents/ui/Spinner.Component";
+import axios from "axios";
+import { baseUrl } from "../constants/api";
 
-const HomePage=()=>{
+const HomePage = () => {
+    const [date, setData] = useState([]);
+    const [loading, setLoading] = useState([]);
+    const [error, setEro] = useState([]);
+
+    useEffect(() => { 
+        setLoading(true);
+        axios(baseUrl);
+            .then((response)=>{
+                setLoading(false);
+                console.log(response.data);
+            })
+            .catch((error)=>{
+                setError("An error occured");
+            });
+    } ,[]);  
+
+
+
     return (
-        <div>
-            <h5>Home Page</h5>
-            <h5>Home Page</h5>
-            <SPinner></SPinner>
-            <h5>Home Page</h5>
-           
+        <div className="container">
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h1>crypto tracking</h1>
+                    <h4>A place to manage all of your trade informations action</h4>
+                    <h4>Do not forget to always using asset and risk management!!!</h4>
+                </div>
+            </div>
+
         </div>
     )
-        
 
-    };
-    export default HomePage;
-    
+
+};
+export default HomePage;
+
